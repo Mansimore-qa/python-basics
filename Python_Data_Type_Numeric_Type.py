ab46293a247dba82055eb7f11696dfddcd7d0248
Mansi More <mansimore.qa@gmail.com>
2026-05-24 13:46:46 +0530

    Added Python data types- Numeric examples

---
+++ Python_Data_Type_Numeric_Type.py
@@ -0,0 +1,79 @@
+# Data type : Numeric Type - int, float, complex
+
+# 1 Integer(int): It is use to store the whole number without decimal.
+print("\n1 Integer(int): It is use to store the whole number without decimal.")
+
+a = 10
+print(a)
+print(type(a))
+
+
+num1 = -10
+num2 = 20
+num3 = 15
+
+# Operations on Integer
+print("\nOperations on Integer")
+print(f"num1 + num2 = {num1 + num2}")
+print(f"num1 - num2 = {num1 - num2}")
+print(f"num1 * num2 = {num1 * num2}")
+print(f"num1 / num2 = {num1 / num2}")
+print(f"num1 // num2 = {num1 // num2}")
+print(f"num1 % num2 = {num1 % num2}")
+print(f"num1 ** num2 = {num1 ** num2}")
+
+# Built-in functions in Integer
+print("\nBuilt-in functions in Integer")
+print(f"abs(num1) = {abs(num1)}")
+print(f"max(num1, num2, num3) = {max(num1, num2, num3)}")
+print(f"min(num1, num2, num3) = {min(num1, num2, num3)}")
+print(f"sum(num1, num2, num3) = {sum([num1, num2, num3])}")
+print(f"divmod(num1, num2) = {divmod(num1, num2)}")
+print(f"pow(num1, num2) = {pow(num1, num2)}")
+
+
+
+
+# 2 Float(float): It is use to store the whole number with decimal.
+print("\n2 Float(float): It is use to store the whole number with decimal.")
+
+b = 10.5
+print(b)
+print(type(b))
+
+num1 = 10.5
+num2 = 20.5
+num3 = 15.5
+
+# Operations on Float
+print("\nOperations on Float")
+print(f"num1 + num2 = {num1 + num2}")
+print(f"num1 - num2 = {num1 - num2}")
+print(f"num1 * num2 = {num1 * num2}")
+print(f"num1 / num2 = {num1 / num2}")
+print(f"num1 // num2 = {num1 // num2}")
+print(f"num1 % num2 = {num1 % num2}")
+print(f"num1 ** num2 = {num1 ** num2}")
+
+# Built-in functions in Float
+print("\nBuilt-in functions in Float")
+print(f"abs(num1) = {abs(num1)}")
+print(f"max(num1, num2, num3) = {max(num1, num2, num3)}")
+print(f"min(num1, num2, num3) = {min(num1, num2, num3)}")
+print(f"sum(num1, num2, num3) = {sum([num1, num2, num3])}")
+print(f"divmod(num1, num2) = {divmod(num2, 0.5)}")
+print(f"pow(num1, num2) = {pow(num1, num2)}")
+
+
+
+
+
+# 3 Complex(complex): It is use to store the whole number with imaginary number.
+print("\n3 Complex(complex): It is use to store the whole number with imaginary number.")
+
+c = 10 + 5j
+# 10 --> real
+# 5 --> imaginary
+print(c)
+print(type(c))
+
