b578df75e335951a4fce9f5beb967271f6348959
Mansi More <mansimore.qa@gmail.com>
2026-05-24 13:21:21 +0530

    Added Python variables practice program.

---
+++ Python_Variables.py
@@ -0,0 +1,44 @@
+#01. Variables
+print("01. Variables : It is use to store to values/ data in memory")
+a = 10
+b = 20
+c = a + b
+
+'''
+here a,b,c all are variables.
+here 10 is the value of variable a.
+here 20 is the value of variable b.
+here a+b is the value of variable c.
+'''
+print(c)
+
+#02. Variables Rules
+print("\n02. Variables Rules")
+'''
+Variable name should start with a letter or underscore, it can contains the number but should not start with number
+'''
+
+# valid variable list:
+var = 10
+var1 = 10
+_var2 = 10
+VAR3 = 10
+
+
+
+
+'''
+#Invalid variable
+1var = 10 # cannot start with number
+my var = 10 # cannot have space
+my-var = 10 # cannot have hypen(-) or special char
+'''
+
+'''
+Best Practice:
+1. Your variable name should be meaningful
+2. Dont use long words for variable like 'My_variable_name_is_this'
+3. Prefer lowercase letters
+4. Use Uppercase for Constants
+'''
+
